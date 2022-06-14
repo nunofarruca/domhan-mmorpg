e5b419f656e65e4200107b1111b112875efd7f2b
Nuno F <nunofarruca@gmail.com>
2022-06-14 21:00:00 +0100

    hello world

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/assets/logo512.png" className="App-logo" alt="logo" />
+        <p>Hello World!!!!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
